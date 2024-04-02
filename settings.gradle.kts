a0e62d46a3701cc7606536ec050ef58542411f2a
Pavlo Shevchenko <pshevchenko@gradle.com>
2024-04-02 18:15:33 +0200

    Bump Develocity plugin to 3.17

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,8 @@
 plugins {
-    id("com.gradle.enterprise") version "3.16.2"
+    id("com.gradle.develocity") version "3.17"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 rootProject.name = "gradle-build-scan-quickstart"
 
-gradleEnterprise {
-    buildScan {
-        server = "<<your Gradle Enterprise instance>>"
-        publishAlways()
-    }
-}
+develocity.server = "<<your Gradle Enterprise instance>>"
